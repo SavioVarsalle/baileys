e35cd3e989459fcf698c02af94160fdcb03070bd
SavioVarsalle <savio.varsalle@live.com>
2024-07-01 14:56:39 -0300

    Editing make-in-memory

--- lib/Store/make-in-memory-store.d.ts
+++ lib/Store/make-in-memory-store.d.ts
@@ -1,9 +1,19 @@
 import type KeyedDB from '@adiwajshing/keyed-db';
 import type { Comparable } from '@adiwajshing/keyed-db/lib/Types';
 import type { Logger } from 'pino';
-import { proto } from '../../WAProto';
+//import { proto } from '../../WAProto';
 import type makeMDSocket from '../Socket';
-import type { BaileysEventEmitter, Chat, ConnectionState, Contact, GroupMetadata, PresenceData, WAMessage, WAMessageCursor, WAMessageKey } from '../Types';
+import type { 
+    BaileysEventEmitter, 
+    Chat, 
+    ConnectionState, 
+    //Contact, 
+    //GroupMetadata, 
+    //PresenceData, 
+    WAMessage, 
+    //WAMessageCursor, 
+    //WAMessageKey 
+    } from '../Types';
 import { Label } from '../Types/Label';
 import { LabelAssociation } from '../Types/LabelAssociation';
 import { ObjectRepository } from './object-repository';
@@ -15,13 +25,13 @@ export declare const waChatKey: (pin: boolean) => {
 export declare const waMessageID: (m: WAMessage) => string;
 export declare const waLabelAssociationKey: Comparable<LabelAssociation, string>;
 export type BaileysInMemoryStoreConfig = {
-    chatKey?: Comparable<Chat, string>;
+    //chatKey?: Comparable<Chat, string>;
     labelAssociationKey?: Comparable<LabelAssociation, string>;
     logger?: Logger;
     socket?: WASocket;
 };
 declare const _default: (config: BaileysInMemoryStoreConfig) => {
-    chats: KeyedDB<Chat, string>;
+    /*chats: KeyedDB<Chat, string>;
     contacts: {
         [_: string]: Contact;
     };
@@ -41,18 +51,18 @@ declare const _default: (config: BaileysInMemoryStoreConfig) => {
     };
     groupMetadata: {
         [_: string]: GroupMetadata;
-    };
+    };*/
     state: ConnectionState;
-    presences: {
+    /*presences: {
         [id: string]: {
             [participant: string]: PresenceData;
         };
-    };
+    };*/
     labels: ObjectRepository<Label>;
     labelAssociations: KeyedDB<LabelAssociation, string>;
     bind: (ev: BaileysEventEmitter) => void;
     /** loads messages from the store, if not found -- uses the legacy connection */
-    loadMessages: (jid: string, count: number, cursor: WAMessageCursor) => Promise<proto.IWebMessageInfo[]>;
+    //loadMessages: (jid: string, count: number, cursor: WAMessageCursor) => Promise<proto.IWebMessageInfo[]>;
     /**
      * Get all available labels for profile
      *
@@ -71,14 +81,14 @@ declare const _default: (config: BaileysInMemoryStoreConfig) => {
      *
      * @returns Label IDs
      **/
-    getMessageLabels: (messageId: string) => string[];
-    loadMessage: (jid: string, id: string) => Promise<proto.IWebMessageInfo | undefined>;
-    mostRecentMessage: (jid: string) => Promise<proto.IWebMessageInfo>;
-    fetchImageUrl: (jid: string, sock: WASocket | undefined) => Promise<string | null | undefined>;
-    fetchGroupMetadata: (jid: string, sock: WASocket | undefined) => Promise<GroupMetadata>;
-    fetchMessageReceipts: ({ remoteJid, id }: WAMessageKey) => Promise<proto.IUserReceipt[] | null | undefined>;
+    //getMessageLabels: (messageId: string) => string[];
+    //loadMessage: (jid: string, id: string) => Promise<proto.IWebMessageInfo | undefined>;
+    //mostRecentMessage: (jid: string) => Promise<proto.IWebMessageInfo>;
+    //fetchImageUrl: (jid: string, sock: WASocket | undefined) => Promise<string | null | undefined>;
+    //fetchGroupMetadata: (jid: string, sock: WASocket | undefined) => Promise<GroupMetadata>;
+    //fetchMessageReceipts: ({ remoteJid, id }: WAMessageKey) => Promise<proto.IUserReceipt[] | null | undefined>;
     toJSON: () => {
-        chats: KeyedDB<Chat, string>;
+        /*chats: KeyedDB<Chat, string>;
         contacts: {
             [_: string]: Contact;
         };
@@ -95,18 +105,18 @@ declare const _default: (config: BaileysInMemoryStoreConfig) => {
                 toJSON: () => proto.IWebMessageInfo[];
                 fromJSON: (newItems: proto.IWebMessageInfo[]) => void;
             };
-        };
+        };*/
         labels: ObjectRepository<Label>;
         labelAssociations: KeyedDB<LabelAssociation, string>;
     };
     fromJSON: (json: {
-        chats: Chat[];
+        /*chats: Chat[];
         contacts: {
             [id: string]: Contact;
         };
         messages: {
             [id: string]: proto.IWebMessageInfo[];
-        };
+        };*/
         labels: {
             [labelId: string]: Label;
         };
